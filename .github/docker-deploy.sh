17ace9901d6bed8eb505bb2fa012220a5d2cca27
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-12 23:23:50 +0700

    Fix attempt 4

--- .github/docker-deploy.sh
+++ .github/docker-deploy.sh
@@ -5,14 +5,11 @@ mkdir -p "${HOME}/.ssh"
 chmod 700 "${HOME}/.ssh"
 printf '%s\n' "$2" > "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa"
-eval $(ssh-agent)
-ssh-add "${HOME}/.ssh/id_rsa"
-printf '%s %s\n' "$5" "$3" > "${HOME}/.ssh/ssh_known_hosts"
 
 echo "Configuring application environment variables"
 echo "SERVER_DB_PASSWORD=$1" > .env.production
 
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}:${5}" pull
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}:${5}" up -d
+docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" pull
+docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d
 
 rm -Rf "${HOME}/.ssh"
