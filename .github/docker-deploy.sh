249e61ec74da38905b6d2e131e34485ffd612d49
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-13 22:20:51 +0700

    Automatic deployment of database

--- .github/docker-deploy.sh
+++ .github/docker-deploy.sh
@@ -5,14 +5,15 @@ mkdir -p "${HOME}/.ssh"
 chmod 700 "${HOME}/.ssh"
 printf '%s\n' "$2" > "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa"
-#eval $(ssh-agent)
-#ssh-add "${HOME}/.ssh/id_rsa"
 printf '%s\n' "$3" > "${HOME}/.ssh/known_hosts"
 
 echo "Configuring application environment variables"
 echo "SERVER_DB_PASSWORD=$1" > .env.production
+WITH_DB=$(cat ./with_db)
+
+DOCKER_SERVICES="fe be ${WITH_DB}"
 
 docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" pull
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d
+docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
 
 rm -Rf "${HOME}/.ssh"
