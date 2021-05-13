8f6ce4b246092328f9eca218b44399317fac283e
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-13 22:53:51 +0700

    Fix script, deploy only

--- .github/db-update.sh
+++ .github/db-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DTFMT=$(date +%Y%m%d-%H%M%S)
-UPDATE_FOLDER="db/updates/${DTFMT}"
-ssh "$1@$2" "mkdir ~/${UPDATE_FOLDER}/stored_procedure"
+UPDATE_FOLDER="${HOME}/db/updates/${DTFMT}"
+ssh "$1@$2" "mkdir -p ${UPDATE_FOLDER}"
 scp ./db/sql/update/update.sql "$1@$2:${UPDATE_FOLDER}/"
 cat ./db/sql/update/stored_procedure/*.sql > stored_procedures.sql
 scp stored_procedures.sql "$1@$2:${UPDATE_FOLDER}/"
@@ -12,9 +12,9 @@ DBPASS=${4}
 DBNAME=${5}
 
 docker -H "ssh://${1}@${2}" exec -it docker-nodejs-mariadb_db_1 sh -c \
-  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < /home/${UPDATE_FOLDER}/update.sql"
+  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${UPDATE_FOLDER}/update.sql"
 
 docker -H "ssh://${1}@${2}" exec -it docker-nodejs-mariadb_db_1 sh -c \
-  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < /home/${UPDATE_FOLDER}/stored_procedures.sql"
+  "mysql --host=localhost --port=3306 --protocol=tcp -u ${DBUSER} -p${DBPASS} ${DBNAME} < ${UPDATE_FOLDER}/stored_procedures.sql"
 
 rm -Rf "${HOME}/.ssh"
