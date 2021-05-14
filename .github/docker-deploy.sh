376626434b77c2a19d4ab84649a349d021331f01
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-14 18:57:58 +0700

    Update deploy script

--- .github/docker-deploy.sh
+++ .github/docker-deploy.sh
@@ -14,4 +14,4 @@ WITH_DB=$(cat ./with_db)
 DOCKER_SERVICES="fe be ${WITH_DB}"
 
 docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" pull
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
+SERVER_DB_PASSWORD="$1" docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
