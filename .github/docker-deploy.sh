120d7ba8d23cb8ce629d22c49b52f86ee904774b
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-14 19:27:14 +0700

    Rebuild dev images

--- .github/docker-deploy.sh
+++ .github/docker-deploy.sh
@@ -7,15 +7,13 @@ printf '%s\n' "$2" > "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa"
 printf '%s\n' "$3" > "${HOME}/.ssh/known_hosts"
 
-echo "Configuring application environment variables"
-echo "SERVER_DB_PASSWORD=$1" > .env.production
 WITH_DB=$(cat ./with_db)
 
 DOCKER_SERVICES="fe be ${WITH_DB}"
 
 ssh "$4@$5" "mkdir -p /home/db/my_data"
 
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" down
+docker-compose -f docker-compose.production.yml -H "ssh://${4}@${5}" down
 docker -H "ssh://${4}@${5}" system prune -a -f
-docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" pull ${DOCKER_SERVICES}
-SERVER_DB_PASSWORD="$1" docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
+docker-compose -f docker-compose.production.yml -H "ssh://${4}@${5}" pull ${DOCKER_SERVICES}
+SERVER_DB_PASSWORD="$1" docker-compose -f docker-compose.production.yml -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
