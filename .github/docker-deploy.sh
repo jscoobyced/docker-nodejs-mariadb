dd28e3d7718af411031af09c03488a222072a5ab
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-05-14 19:17:14 +0700

    Try to get volumes mounted remotely

--- .github/docker-deploy.sh
+++ .github/docker-deploy.sh
@@ -13,6 +13,8 @@ WITH_DB=$(cat ./with_db)
 
 DOCKER_SERVICES="fe be ${WITH_DB}"
 
+ssh "$1@$2" "mkdir -p /home/db/my_data"
+
 docker -H "ssh://${4}@${5}" system prune -a -f
 docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" pull ${DOCKER_SERVICES}
 SERVER_DB_PASSWORD="$1" docker-compose -f docker-compose.production.yml --env-file .env.production -H "ssh://${4}@${5}" up -d ${DOCKER_SERVICES}
